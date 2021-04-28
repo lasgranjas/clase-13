b307ad20716830983ad3f550b64e65103711d112
lasgranjas <78686390+lasgranjas@users.noreply.github.com>
2021-04-28 06:45:15 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,66 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 4 4 4 4 . . . . . . 
+        . . . . 4 4 4 5 5 4 4 4 . . . . 
+        . . . 3 3 3 3 4 4 4 4 4 4 . . . 
+        . . 4 3 3 3 3 2 2 2 1 1 4 4 . . 
+        . . 3 3 3 3 3 2 2 2 1 1 5 4 . . 
+        . 4 3 3 3 3 2 2 2 2 2 5 5 4 4 . 
+        . 4 3 3 3 2 2 2 4 4 4 4 5 4 4 . 
+        . 4 4 3 3 2 2 4 4 4 4 4 4 4 4 . 
+        . 4 2 3 3 2 2 4 4 4 4 4 4 4 4 . 
+        . . 4 2 3 3 2 4 4 4 4 4 2 4 . . 
+        . . 4 2 2 3 2 2 4 4 4 2 4 4 . . 
+        . . . 4 2 2 2 2 2 2 2 2 4 . . . 
+        . . . . 4 4 2 2 2 2 4 4 . . . . 
+        . . . . . . 4 4 4 4 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 50, 50)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    projectile.destroy(effects.fire, 500)
+    sprite_2.destroy(effects.fountain, 500)
+    mySprite.say("Bien!!!", 500)
+})
+let projectile: Sprite = null
+let sprite_2: Sprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a 1 1 1 a a a a 1 1 1 a a a a 
+    a a 1 1 1 a a a a 1 1 1 a a a a 
+    a a f 1 1 a a a a f 1 1 a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a 3 3 3 3 a a a a a a a 
+    a a a a a 3 3 3 3 a a a a a a a 
+    a a a a a 3 3 3 3 a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    a a a a a a a a a a a a a a a a 
+    `, SpriteKind.Player)
+sprite_2 = sprites.create(img`
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 5 5 5 1 1 1 1 5 5 5 1 1 1 1 
+    1 1 5 5 5 1 1 1 1 5 5 5 1 1 1 1 
+    1 1 f 5 5 1 1 1 1 f 5 5 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 4 4 4 4 1 1 1 1 1 1 1 
+    1 1 1 1 1 4 4 4 4 1 1 1 1 1 1 1 
+    1 1 1 1 1 4 4 4 4 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    `, SpriteKind.Enemy)
+controller.moveSprite(mySprite)
+sprite_2.setPosition(126, 93)
